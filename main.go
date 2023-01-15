1bc89b6a3ed9135ee0762de32100d872266acf95
Jens Drenhaus <jens.drenhaus@9elements.com>
2023-01-15 18:40:25 +0100

    bug fix from 0.2 in 0.3
    Signed-off-by: Jens Drenhaus <jens.drenhaus@9elements.com>

--- main.go
+++ main.go
@@ -5,6 +5,6 @@ import "fmt"
 func main() {
 	fmt.Println("Hello!")
 	fmt.Println("This is version v0.1.0")
-	fmt.Println("This is version v0.2.0 (with a bug)")
+	fmt.Println("This is version v0.2.0")
 	fmt.Println("This is version v0.3.0")
 }
