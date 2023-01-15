f4b807a2c315c30b018cb84dc968b690aeb3da67
Jens Drenhaus <jens.drenhaus@9elements.com>
2023-01-15 18:43:11 +0100

    some changes in 0.3
    Signed-off-by: Jens Drenhaus <jens.drenhaus@9elements.com>

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello!")
+	fmt.Println("Hello WORLD!")
 	fmt.Println("This is version v0.1.0")
 	fmt.Println("This is version v0.2.0")
 	fmt.Println("This is version v0.3.0")
